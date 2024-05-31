24d99f8f93ba8f0bbeb34296d89114b113f8b426
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-31 14:27:49 +0300

    M15U01Ex002

---
+++ Module15/M15U01Ex002/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace M15U01Ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Волга", "Москвич", "Нива", "Газель" };
+            string[] buses = { "Газель", "Икарус", "ЛиАЗ" };
+
+            var microBuses=cars.Intersect(buses);
+
+            foreach(string s in microBuses)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
