7ec3aa6bdf96c33e35d16cada4d582c5c60846c0
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-31 15:26:11 +0300

    M15U01Ex06

---
+++ Module15/M15U01Ex06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace M15U01Ex06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            var text = Console.ReadLine().ToCharArray();
+
+            char[]chars = {' ','.',',','!','?',':',';'};
+
+            if (text.Length>0)
+            {
+                var uniqueSymbols = text.Distinct().Except(chars);
+
+                Console.Write($"Уникальные символы ({uniqueSymbols.Count()} шт): ");
+                foreach (var ch in uniqueSymbols)
+                {
+                    Console.Write(ch + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Пустая строка");
+            }
+        }
+    }
+}
