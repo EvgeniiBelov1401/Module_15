535a83af4b3c6b7bc616a833c8541e48b67151de
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-05-31 14:21:14 +0300

    M15U01Ex001

---
+++ Module15/M15U01Ex001/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace M15U01Ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] drinks = {"Вино","Кока-Кола","Пиво","Лимонад","Сок"};
+            string[] alcohol = { "Вино", "Пиво" };
+
+            var drinksForKids=drinks.Except(alcohol);
+
+            foreach (var item in drinksForKids)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
