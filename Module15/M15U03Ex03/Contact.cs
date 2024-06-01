c787b5f789197197707b19d83b2a2e079aa88e3f
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-01 19:31:09 +0300

    M15U03Ex03

---
+++ Module15/M15U03Ex03/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M15U03Ex03
+{
+    internal class Contact
+    {
+        public string Name { get; set; }
+        public long Phone { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string name,long phone,string email)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+        }
+    }
+}
---
+++ Module15/M15U03Ex03/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace M15U03Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>
+            {
+            new Contact("Игорь", 79990000001, "igor@example.com"),
+            new Contact("Сергей", 79990000010, "serge@example.com"),
+            new Contact("Анатолий", 79990000011, "anatoly@example.com"),
+            new Contact("Валерий", 79990000012, "valera@example.com"),
+            new Contact("Сергей", 799900000013, "serg@gmail.com"),
+            new Contact("Иннокентий", 799900000013, "innokentii@example.com")
+            };
+
+            var groupingByEmail=phoneBook.GroupBy(x => x.Email.Split('@').Last());
+
+            foreach (var grouping in groupingByEmail)
+            {
+                if (grouping.Key.Contains("example"))
+                {
+                    Console.WriteLine("Фейковые адреса: ");
+                    foreach (var contact in grouping)
+                    {
+                        Console.WriteLine(contact.Name+" - "+contact.Email);
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Реальные адреса: ");
+                    foreach (var contact in grouping)
+                    {
+                        Console.WriteLine(contact.Name + " - " + contact.Email);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
