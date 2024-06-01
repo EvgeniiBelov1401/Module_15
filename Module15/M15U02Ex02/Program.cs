eb76ff213ff26b5a53725977a657fbe7f0fa4bb3
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-01 17:15:35 +0300

    M15U02Ex02

---
+++ Module15/M15U02Ex02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace M15U02Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contacts = new List<Contact>()
+            {
+               new Contact() { Name = "Андрей", Phone = 79994500508 },
+               new Contact() { Name = "Сергей", Phone = 799990455 },
+               new Contact() { Name = "Иван", Phone = 79999675334 },
+               new Contact() { Name = "Игорь", Phone = 8884994 },
+               new Contact() { Name = "Анна", Phone = 665565656 },
+               new Contact() { Name = "Василий", Phone = 3434 }
+            };
+
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine(contact.Name+" - "+contact.Phone);
+            }
+
+            Console.WriteLine($"Неверных контактов: {contacts.Count(x=>x.Phone.ToString().Length!=11 ||
+            !x.Phone.ToString().StartsWith('7'))} шт");
+        }
+    }
+}
