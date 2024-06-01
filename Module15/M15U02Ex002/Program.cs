ea03cd63c613679bd6ee68863f9d3a0f08e239f7
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-01 16:58:19 +0300

    M15U02Ex002

---
+++ Module15/M15U02Ex002/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace M15U02Ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           while (true)
+            {
+                Random random = new Random();
+                var studentsJournal = new List<Students>
+            {
+                new Students("Алексей",random.Next(18,30)),
+                new Students("Иван",random.Next(18,30)),
+                new Students("Ирина",random.Next(18,30)),
+                new Students("Павел",random.Next(18,30)),
+                new Students("Юлия",random.Next(18,30)),
+                new Students("Петр",random.Next(18,30)),
+                new Students("Анна",random.Next(18,30)),
+                new Students("Мария",random.Next(18,30)),
+                new Students("Андрей",random.Next(18,30)),
+                new Students("Семен",random.Next(18,30)),
+            };
+                foreach (var student in studentsJournal)
+                {
+                    Console.WriteLine(student.Name+" - "+student.Age+" лет.");
+                }
+
+                Console.WriteLine($"\nМладше 25 лет: {studentsJournal.Count(x=>x.Age<25)} студентов.");
+
+                Console.WriteLine();
+                Console.ReadLine(); 
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Module15/M15U02Ex002/Students.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M15U02Ex002
+{
+    internal class Students
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Students(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
