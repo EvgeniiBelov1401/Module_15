d34bf73d9ad415e75831b3f9572b9f462c9f25af
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-01 18:20:54 +0300

    M15U02Ex08

---
+++ Module15/M15U02Ex08/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace M15U02Ex08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var numbers = new List<int>();
+                try
+                {
+                    Console.Write("Введите длину списка: ");
+                    if (int.TryParse(Console.ReadLine(),out int listCount))
+                    {
+                        
+                        for (int i = 0;i< listCount; i++)
+                        {
+                            Console.Write($"Число №{i+1}: ");
+                            if (int.TryParse(Console.ReadLine(), out int number))
+                            {
+                                numbers.Add(number);
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Введите верное число");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Введите верное число");
+                    }
+
+                    Console.WriteLine($"\nДлина списка: {numbers.Count()}\n" + $"" + $"" + $"Сумма всех чисел: {numbers.Sum()}\n" + $"" + $"" + $"Максимальное число: {numbers.Max()}\n" + $"Минимальное число: {numbers.Min()}\n" + $"Среднее число: {numbers.Average()}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+
+
+                Console.WriteLine();
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+    }
+}
