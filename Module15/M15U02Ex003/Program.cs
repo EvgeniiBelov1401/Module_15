7b37f1421e62b7a53627cfe8259793e81484bd06
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-01 17:20:34 +0300

    M15U02Ex003

---
+++ Module15/M15U02Ex003/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace M15U02Ex003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           while (true)
+            {
+                Random random = new Random();
+                var numbers = new[] { random.Next(1, 9), random.Next(1, 9), random.Next(1, 9) };
+
+                foreach ( var number in numbers )
+                {
+                    Console.Write(number+" ");
+                }
+                Console.WriteLine("\nСумма: "+numbers.Sum());
+
+                Console.WriteLine();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
