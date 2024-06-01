e08cfaef2045bcd31c872bcc1d29c62b2aa99b56
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-01 16:25:23 +0300

    M15U02Ex001

---
+++ Module15/M15U02Ex001/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace M15U02Ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            int result = numbers.Aggregate((x,y)=>x-y);
+
+            Console.WriteLine(result);
+        }
+    }
+}
