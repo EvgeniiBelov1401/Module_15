3cb872eba147e517a105468f5e6e37ea3f73b193
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-01 17:35:50 +0300

    M15U02Ex03

---
+++ Module15/M15U02Ex03/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace M15U02Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+                var numbers = new List<int>
+                {
+                    random.Next(1,10),
+                    random.Next(1,10),
+                    random.Next(1,10),
+                    random.Next(1,10),
+                    random.Next(1,10),
+                    random.Next(1,10),
+                    random.Next(1,10),
+                    random.Next(1,10),
+                    random.Next(1,10),
+                    random.Next(1,10),
+                };
+
+                foreach (var number in numbers)
+                {
+                    Console.Write(number+" ");
+                }
+                var average =(double) numbers.Sum() / numbers.Count();
+                
+                Console.WriteLine($"\nСреднее арифметическое: {average}");
+                Console.WriteLine($"\nСреднее арифметическое: {numbers.Average()}");
+                
+                
+                Console.WriteLine();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
