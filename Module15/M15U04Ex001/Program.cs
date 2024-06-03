3c0cccb0bac01e1e7d276069283a88d7fc0a7152
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-03 11:31:45 +0300

    M15U04Ex001

--- Module15/M15U04Ex001/Program.cs
+++ Module15/M15U04Ex001/Program.cs
@@ -6,10 +6,17 @@
         {
             var letters = new string[] { "A", "B", "C", "D", "E" };
             var numbers = new int[] { 1, 2, 3 };
+            var numbers2 = new int[] { 1, 2, 3 };
 
             var zip=letters.Zip(numbers,(l,n)=>l+n.ToString());
 
             foreach (var i in zip)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            var zip2=numbers.Zip(numbers2,(n,n2)=>n+n2);
+            foreach (var i in zip2)
             {
                 Console.WriteLine(i);
             }
