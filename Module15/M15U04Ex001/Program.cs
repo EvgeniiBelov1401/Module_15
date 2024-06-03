c8ba1e6ffda3e40fcfce75a27e304aa3ea1044d4
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-03 11:12:53 +0300

    M15U04Ex001

---
+++ Module15/M15U04Ex001/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace M15U04Ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var letters = new string[] { "A", "B", "C", "D", "E" };
+            var numbers = new int[] { 1, 2, 3 };
+
+            var zip=letters.Zip(numbers,(l,n)=>l+n.ToString());
+
+            foreach (var i in zip)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
