d589a4e0e29d799718219fc6218b3f8dfa937558
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-05 14:20:39 +0300

    FinalExercise

--- Module15/FinalExercise/Program.cs
+++ Module15/FinalExercise/Program.cs
@@ -16,10 +16,25 @@ namespace FinalExercise
                new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
                new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
            };
+            while (true)
+            {
+                for (int i = 0; i < classes.Length; i++)
+                {
+                    var oneClass=classes.ToList()[i].Students;                    
+                    Console.WriteLine($"Класс {i + 1}: "+string.Join(" ",oneClass));
+                }
 
-            var allStudents = GetAllStudents(classes);
+            
+                Console.WriteLine("\n\nСобрать всех учеников в одном классе (нажмите ENTER)");
+                Console.ReadLine();
 
-            Console.WriteLine(string.Join(" ", allStudents));
+                var allStudents = GetAllStudents(classes);
+                Console.WriteLine("Актовый зал: " + string.Join(" ", allStudents));
+
+                Console.WriteLine();
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
     }
 }
