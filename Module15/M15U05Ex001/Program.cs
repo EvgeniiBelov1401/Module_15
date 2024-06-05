8602ca708e7842c8f8ff39cc40e9e3306b5d88c6
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-05 12:42:25 +0300

    M15U05Ex001

---
+++ Module15/M15U05Ex001/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace M15U05Ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>
+            {
+               new Student {Name="Андрей", Age=23 },
+               new Student {Name="Сергей", Age=27 },
+               new Student {Name="Дмитрий", Age=29 }
+            };
+
+            var youngStudents = students.Where(x => x.Age < 25);
+            Console.WriteLine(youngStudents.Count());
+
+            students.Add(new Student { Name = "Анна", Age = 21 });
+
+            foreach (var student in youngStudents)
+            {
+                Console.WriteLine(student.Name);
+            }
+            Console.WriteLine(youngStudents.Count());
+        }
+    }
+}
