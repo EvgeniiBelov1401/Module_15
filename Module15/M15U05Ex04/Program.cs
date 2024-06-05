61e90b3922007d7e17d1508f0cc0dd639fce9249
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-05 13:04:39 +0300

    M15U05Ex04

---
+++ Module15/M15U05Ex04/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace M15U05Ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            var evenNumbers = numbers.Where(x=>x%2==0).ToList();
+            numbers.Add(12);
+
+            foreach(int x in evenNumbers)
+            {
+                Console.Write(x+" ");
+            }
+        }
+    }
+}
