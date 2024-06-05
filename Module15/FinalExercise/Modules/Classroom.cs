785d7c6c7f5b046d13e1846878f7867c610d754e
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-06-05 13:52:34 +0300

    FinalExercise

---
+++ Module15/FinalExercise/Modules/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExercise.Modules
+{
+    internal class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Module15/FinalExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using FinalExercise.Modules;
+
+namespace FinalExercise
+{
+    internal class Program
+    {
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(c => c.Students).ToArray();
+        }
+        static void Main(string[] args)
+        {
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+    }
+}
